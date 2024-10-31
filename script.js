55e43951c629355873b44a9a62c664e5f69c928a
JughoDorf <cartoonplay110@gmail.com>
2024-10-31 23:19:54 +1200

    1
    1

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.getElementById('requestForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const department = document.getElementById('department').value;
+    const contact = document.getElementById('contact').value;
+    const issue = document.getElementById('issue').value;
+    const description = document.getElementById('description').value;
+
+    alert(`Заявка от ${name} из отдела ${department} успешно отправлена! Проблема: ${issue}`);
+});
