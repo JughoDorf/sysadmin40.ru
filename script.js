d378795ed06c503766fe4a583159fbc00c49c904
JughoDorf <cartoonplay110@gmail.com>
2024-11-01 00:20:30 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
 document.getElementById('requestForm').addEventListener('submit', function (event) {
-    event.preventDefault();
+    event.preventDefault(); // предотвращает перезагрузку страницы
 
     const name = document.getElementById('name').value;
-    const department = document.getElementById('department').value;
+    const className = document.getElementById('class').value; // изменено с department на class
     const issue = document.getElementById('issue').value;
     const description = document.getElementById('description').value;
 
     alert(`Спасибо, ${name}! Ваша заявка отправлена на рассмотрение.`);
 });
+
