4d39810dbacfb2b9b711454f595ed382c388497d
JughoDorf <cartoonplay110@gmail.com>
2024-11-05 23:25:19 +1200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,16 @@
-document.getElementById('applicationForm').addEventListener('submit', function(e) {
-    e.preventDefault(); // предотвращаем стандартное поведение формы
+function showLoading() {
+  document.getElementById('loading').style.display = 'flex';
+}
 
-    // Здесь добавьте код для отправки данных в Google Форму
-    const formData = new FormData(this);
-    const url = 'https://docs.google.com/forms/d/e/1FAIpQLSdpQoFwSu-YUilChpejwLukqsuj4itHOIkvrRNI13mM6vUVhQ/formResponse';
+function hideLoading() {
+  document.getElementById('loading').style.display = 'none';
+}
 
-    fetch(url, {
-        method: 'POST',
-        mode: 'no-cors',
-        body: formData,
-    })
-    .then(() => {
-        // Показываем сообщение об успешной отправке
-        document.getElementById('successMessage').classList.remove('hidden');
-        this.reset(); // очищаем форму
-    })
-    .catch(error => console.error('Ошибка:', error));
-});
+function showSuccessModal() {
+  hideLoading();
+  document.getElementById('successModal').style.display = 'block';
+}
+
+function closeModal() {
+  document.getElementById('successModal').style.display = 'none';
+}
