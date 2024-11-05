72a075a7e2f75b39045dc179604f7f5e75da9398
JughoDorf <cartoonplay110@gmail.com>
2024-11-06 00:33:37 +1200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,18 +2,15 @@
 function showLoading() {
     document.getElementById("loading").style.display = "flex";
 }
-
 // Функция для показа модального окна
 function showSuccessModal() {
     document.getElementById("loading").style.display = "none";
     document.getElementById("successModal").style.display = "flex";
 }
-
 // Функция для закрытия модального окна
 function closeModal() {
     document.getElementById("successModal").style.display = "none";
 }
-
 // Функция для авто-расширения textarea
 function autoResize(textarea) {
     textarea.style.height = "auto";
