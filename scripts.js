0306a1b8c3c0aec3a9ae2ad388ea69df5296ab5e
JughoDorf <cartoonplay110@gmail.com>
2024-11-05 23:27:05 +1200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,13 @@
 function showLoading() {
   document.getElementById('loading').style.display = 'flex';
 }
-
 function hideLoading() {
   document.getElementById('loading').style.display = 'none';
 }
-
 function showSuccessModal() {
   hideLoading();
   document.getElementById('successModal').style.display = 'block';
 }
-
 function closeModal() {
   document.getElementById('successModal').style.display = 'none';
 }
