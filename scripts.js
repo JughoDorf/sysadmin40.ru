bbf9b782b11474f1fc475189b263828f8f6d19af
JughoDorf <cartoonplay110@gmail.com>
2024-11-12 20:52:55 +1200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 // Показать загрузочный экран
-function showLoading() {
+function showLoading(event) {
+  event.preventDefault();  // Предотвращаем стандартную отправку формы
   document.getElementById('loading').style.display = 'flex'; // Показываем экран загрузки
 
   // Задержка перед отправкой формы (например, 2 секунды)
   setTimeout(function() {
-    document.forms[0].submit(); // Отправляем форму
+    document.getElementById('myForm').submit();  // Отправляем форму вручную через JavaScript
   }, 2000); // 2000 миллисекунд = 2 секунды
 }
 
@@ -22,4 +23,10 @@ function showSuccessModal() {
 // Закрыть модальное окно
 function closeModal() {
   document.getElementById('successModal').style.display = 'none';
+}
+
+// Автоизменение размера textarea
+function autoResize(textarea) {
+  textarea.style.height = 'auto';  // Сброс высоты, чтобы вычислить новую
+  textarea.style.height = (textarea.scrollHeight) + 'px'; // Устанавливаем высоту на основе содержимого
 }
